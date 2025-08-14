5c08ab1089398e68f4198eb377970a821aac47fa
Raveena Dhakar <ravinadhakar351@gmail.com>
2025-08-15 00:28:10 +0530

    setup of basic frontend-backend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{message || "hello frontend"}</h1>
+    </div>
+  );
+}
+
+export default App;
